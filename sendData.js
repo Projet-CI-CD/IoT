8e72c00afd882860f2245d73282905908ce67783
SajedehAdelia <adelia.fathipour@gmail.com>
2025-04-22 10:46:13 +0200

    Add sendData.js and .gitignore

---
+++ sendData.js
@@ -0,0 +1,25 @@
+const axios = require("axios");
+const fs = require("fs");
+
+const API_URL = "http://localhost:3000/data";
+
+const rawData = fs.readFileSync("iot_devices.json");
+const sensorData = JSON.parse(rawData);
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function sendData() {
+  for (const data of sensorData) {
+    try {
+      const response = await axios.post(API_URL, data);
+      console.log(`Sent ${data.device_id} → Status: ${response.status}`);
+    } catch (err) {
+      console.error(` Error sending ${data.device_id}:`, err.message);
+    }
+    await delay(500); 
+  }
+}
+
+sendData();
