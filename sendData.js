7843542f9b123d8efaad86844789fdef04c278fa
Maxence COEUR <maxence.coeur@etu.univ-lyon1.fr>
2025-05-19 13:58:50 +0200

    changeadress

--- sendData.js
+++ sendData.js
@@ -1,7 +1,7 @@
 const axios = require("axios"); 
 const { simulateTemperature, simulateHumidity } = require("./fonctions");
 
-const API_URL = "http://10.70.5.208:3000/iot";
+const API_URL = "https://api-749292095840.europe-west9.run.app/iot";
 
 const devices = [];
 
